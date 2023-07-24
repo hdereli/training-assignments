ef933ad274e15939782bac91d594a2bbf367ebd5
hdereli <hdereli@gmail.com>
2023-07-24 10:10:35 +0300

    assignment 01

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # training-assignments
-Training Assignments
+
+### Assignment 01
+- data.js içerisinde bulunan veri seti üzerinde istenilen işlemleri yapınız.
+- sonuçların console'a loglanması yeterlidir
---
+++ assignment-01/main.js
@@ -0,0 +1,10 @@
+// -------------------------------
+// Data Set
+// -------------------------------
+console.log("Dataset");
+console.log(testData);
+
+// -------------------------------
+// Starting Point
+// -------------------------------
+console.log("Starting Point");
